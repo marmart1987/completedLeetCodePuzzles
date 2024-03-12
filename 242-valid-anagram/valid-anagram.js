48872f46696dc81509d5367acf95a975f5c72345
marmart1987 <marmtk1811@gmail.com>
2024-03-13 06:43:54 +0800

    Time: 75 ms (48.60%) | Memory: 53.9 MB (27.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,16 +4,7 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = Array.from(s)
-    t = Array.from(t)
-    if(s.length !== t.length){return false}
-    for ( i in s){
-      let index = t.indexOf(s[i])    
-      if( index === -1){
-        return false
-      }else{
-        t.splice(index,1)
-      }
-    }
-    return true
+     s = s.split('').sort().join()
+    t = t.split('').sort().join()
+    return s == t
 };
