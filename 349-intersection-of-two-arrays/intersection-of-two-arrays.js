c93435f02186ebb65e1e47bf1f6131872d821bda
marmart1987 <marmtk1811@gmail.com>
2024-03-13 06:23:35 +0800

    Time: 55 ms (57.53%) | Memory: 49.6 MB (78.89%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  let result = [];
+  for (i in nums1)  {
+    if((nums2.indexOf(nums1[i]) !== -1 ) && result.indexOf(nums1[i]) === -1 ){
+result.push(nums1[i])
+    }
+  }
+  return result
+};
