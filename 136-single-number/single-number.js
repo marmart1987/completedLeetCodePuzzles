b186c28b7023eb55edea403533f6b28eed5eb9cb
marmart1987 <marmtk1811@gmail.com>
2024-03-27 13:41:06 +0800

    Time: 271 ms (13.33%) | Memory: 52.8 MB (36.97%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -5,12 +5,10 @@
 var singleNumber = function(nums) {
     for (let i in nums){
         let lastIndex = nums.lastIndexOf(nums[i]) 
-        //console.log(lastIndex, i)
         if(lastIndex == i){
             return nums[i]
         }else{
             nums.splice(lastIndex,1)
         }
-       // console.log(nums)
     }
 };
