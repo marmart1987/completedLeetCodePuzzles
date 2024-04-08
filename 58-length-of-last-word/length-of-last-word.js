06e45473924324f0035c094ff25943426782e0e5
marmart1987 <marmtk1811@gmail.com>
2024-04-08 18:23:00 +0800

    Time: 52 ms (51.59%) | Memory: 48.5 MB (84.92%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,7 +5,6 @@
 var lengthOfLastWord = function(s) {
     s = s.trim()
     let len = 0
-    console.log(s,s.length-1)
     let i = s.length-1
     while (true){
         if(s[i] === " " || i === -1){
